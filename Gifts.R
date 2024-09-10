bc385246baf963fd997790f2181b1d4e1526a094
eddmunyiri@gmail.com <eddmunyiri@gmail.com>
2024-09-10 16:55:33 +0300

    plts visualizations

--- Gifts.R
+++ Gifts.R
@@ -18,3 +18,49 @@ summary(gifts_df)
 #Data Cleaning
 #Removing Null values
 
+#first converting to the desired data formats
+gifts_df <- gifts_df %>%
+  mutate(GiftDate = as.Date(GiftDate, format = "%Y-%m-%d"))
+
+gifts_cleaned <- gifts_df %>%
+  mutate(
+    across(where(is.character), ~if_else( . == "NULL", str_c("user", AccountID, "mising", sep = "_"), .))
+  )
+
+head(gifts_cleaned)
+
+#Visualizations
+
+#ensuring the 'Amountid' is treated as a unique_factor
+gifts_cleaned$AccountID <- as.factor(gifts_cleaned$AccountID)
+
+#visualizing a bar plot for all members
+barplot(amounts,
+        main = "The gifts amount received by all members",
+        xlab = "AccountID",
+        ylab = "Amount",
+        col = c('skyblue'),
+        las = 2, 
+        cex.names = 0.8) 
+
+
+#visualizing the amount offered, distinguishing between  members and  non_members
+gifts_cleaned$Member <- ifelse(gifts_cleaned$Member == "Y", "Member", "Non-member" )
+
+ggplot(gifts_cleaned, aes(x = AccountID, y = Amount, fill = Member ))+
+  geom_bar(stat = 'identity', position = 'stack')+
+  scale_fill_manual(values = c("Member" = "green", "Non-member" = "red"))+
+  labs(title = "The gifts amount received by members and non-members",
+     x = "AccountID",
+     y = "Amount") +
+  theme(axis.text.x = element_text(angle = 90, hjust = 1))
+
+
+
+
+
+
+
+
+
+
--- Promotions.R
+++ Promotions.R
@@ -16,4 +16,29 @@ str(promotions_df)
 summary(promotions_df)
 
 #Data Cleaning
+#converting the date column to the desired datatype
+promotions_df <- promotions_df %>%
+  mutate(MailDate = as.Date(MailDate, format = "%Y-%m-%d"))
+  
+promotions_cleaned <- promotions_df %>%
+  mutate(across(where(is.character), ~ if_else(. == "NULL", str_c("user", AccountID, "missing", sep = "_"), .)))
 
+head(promotions_cleaned)
+
+#Visualizations
+#Visualizing the different channels used by members
+
+#ensuring the accountId is treated as a factor
+promotions_cleaned$AccountID <- as.factor(promotions_cleaned$AccountID)
+
+#Visualizing the channells used by distinguished members
+
+promotions_cleaned$Member <- ifelse(promotions_cleaned$Member == "Y", 'Member', "Non_member")
+
+ggplot(promotions_cleaned, aes(x = AccountID, y = Channel, fill = Member))+
+  geom_bar(stat = "identity", position = "stack")+
+  scale_fill_manual(values = c(Member = "green", Non_member = "red"))+
+  labs(title = "Channels used by Members and Non_members",
+       x="AccountId", y="channels")+
+  theme(axis.text.x = element_text(angle = 90, hjust = 1))
+  
